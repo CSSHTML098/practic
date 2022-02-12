e9e63d223678198f0c01039a0c0df7d4f03c5b5b
CSSHTML098 <92036900+CSSHTML098@users.noreply.github.com>
2022-02-12 14:15:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,168 @@
+// #1
+// let k = +prompt('Enter k')
+// let n = +prompt('Enter n (n > 0)')
+// for(let i = k; i <= n; i++){
+//     console.log(k)
+// }
+
+
+// #2
+// let a = +prompt('Первое число')
+// let b = +prompt('Второе число')
+// let n = (b-a)+1
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         console.log(i)
+//     }
+//     console.log(`Количество цифр между ними = ${n}`)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+
+
+3
+
+
+// #4
+// let summ = +prompt('Enter price')
+// for(let i = 1; i <= 10; i++){
+//     console.log(`Price for ${i}kg is ${summ * i}`)
+// }
+
+
+// #5
+// let a = +prompt('Цену за 1 кг конфет');
+// for ( i = 0.1; i <= 1 ; i += 0.1 ){
+//     alert(`Стоимость за ${i.toFixed(1)} кг, я должен заплатить ${(a*i).toFixed(1)}`)
+// }
+// console.log(a)
+// console.log(i)
+
+
+// #6
+// let c = +prompt('Цена за 1кг')
+// for(let a = 1.2; a<=2; a+=0.2){
+//     let s = c*a
+//     console.log(`Цена за ${a.toFixed(1)} кг = ${Math.round(s)} грн`)
+//     console.log('-------------------------')
+// }
+
+
+// #7
+// let a = +prompt('Меньшее число:')
+// let b = +prompt('Ьольшее число:')
+// let c = 0;
+// for (i = a ; i <= b; i++){
+//     c += i;
+// }
+// alert(c)
+// console.log(a)
+// console.log(b)
+
+
+
+// #8 
+// let a = +prompt('Первое число')
+// let b = +prompt('Второе число')
+// k=1
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         k*=i
+        
+        
+//     } console.log(k)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+
+
+// #9
+// let A = +prompt('Enter first number')
+// let B = +prompt('Enter last number')
+// let kvadr = 0
+// for(let i = A; i <= B; i++){
+//     kvadr += (i ** 2)  
+// }
+// console.log(`Number square of all number is ${summ}`)
+
+
+// #10
+// let n = +prompt("Число n")
+//     a = 1;
+//     s = 0;
+// for(s; a<=n; a++){
+//     s+=(1/a);
+// }
+// console.log(`Сумма равняеется ${s}`)
+
+
+// #11
+// let n = +prompt("Число n")
+//     a = 0;
+//     s = 0;
+// for(s; a<=n; a++){
+//     s+=(n + a)**2;
+// }
+// console.log(`Заданная сумма равняеется ${s}`)
+
+
+// #12
+// let a = +prompt('Число:');
+//     c = 1;
+// for (i = 1.1; i <= a; i += 0.1){
+//     c *= i;
+// }
+// alert(`Сумма ряда равна ${c}`);
+// console.log(c)
+// console.log(a)
+
+
+// #13
+// let n = +prompt('Ввидите число');
+// let first = 1.1
+// let second = 1.2
+// let summ = 0
+// let razn = 0
+// for(let i = 1; i <= n; i += 2){
+//     summ += first
+//     first += 0.2 
+// }
+// for(let j = 2; j <= N; j += 2){
+//     razn -= second
+//     second += 0.2 
+// }
+// console.log(`Сума чисел ${summ + razn}`);
+
+
+// #14
+// let n = +prompt("Число n")
+//     s = 0;
+//     k = 1;
+// for(n; k<=n; k++){
+//     s+=(2*k - 1);
+//     console.log(`${k} в квадрате: ${s}`)
+// }
+
+
+
+// #15
+// let a = +prompt("Значение числа а")
+//     n = +prompt("Степень числа а")
+// for(n; n<0; n=n){
+    
+// }
+// console.log(a**n)
+
+
+// #16
+// let a = +prompt('Число')
+// let n = +prompt('Степень')
+// k = 1
+// for(i=1;i<=n;i++){
+//     k = a**i
+//     console.log('A в степени N = ' + k)
+//     console.log('-------------------------')
+// }
+
